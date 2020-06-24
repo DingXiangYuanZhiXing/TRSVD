1b42f9bfa82204ceb94b917882892749ff672022
DingXiangYuanZhiXing <28868457+DingXiangYuanZhiXing@users.noreply.github.com>
2020-06-24 23:52:32 +0800

    Update testAlgorithm1.m

--- testAlgorithm1.m
+++ testAlgorithm1.m
@@ -2,15 +2,19 @@ function [label, Fro_norm] = testAlgorithm1(J, M, U_k, C)
 % testing process for Traditional Matrix PCA Method
 % Input:
 %   J   - ln*1 matrix
-%   M   - ln*1 tensor
-%   A   - ln*N tensor
+%   M   - ln*1 matrix
+%   A   - ln*N matrix
 %   U_k - ln*k 'econ' form
-%   C   - k*1 tensor
+%   c   - k*1 matrix
 % Output:
 %   label    - return the label of closest image
 %   Fro_norm - return the frobenius norm of closest image
-t = (J-M)(:);
+
+t = J - M;
+t = t(:);
+
 c = U_k' * t;
+
 Fro_norm = +Inf; % the minimum forbenius norm 
 label = 0; % the label of minimum norm image
 for j = 1:N
