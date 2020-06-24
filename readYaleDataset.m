8a3510c5a940229c63c089020f873855263fc65c
DingXiangYuanZhiXing <28868457+DingXiangYuanZhiXing@users.noreply.github.com>
2020-06-24 23:24:48 +0800

    Update readYaleDataset.m

--- readYaleDataset.m
+++ readYaleDataset.m
@@ -1,27 +1,35 @@
-function readYaleDataset(dataDir , saveName) %
-    dirs = dir(dataDir);  
-    data = [];  
-    labels = [];  
-    for i = 3:numel(dirs)  
-        imgDir = dirs(i).name;  
-        imgDir = fullfile(dataDir, imgDir);  
-        imgList = dir(fullfile(imgDir, '*.pgm'));  % 
-        for j = 1:20  %numel(imgList)
-            imgName = imgList(j).name;  
-            if strcmp('Ambient.pgm',  imgName(end-10:end))  
-                continue;  
-            end  
-            im = imread(fullfile(imgDir, imgName));  
-            if size(im, 3) ==3  
-                im = rgb2gray(im);  
-            end  
-            im = imresize(im, [32 32]);  
-            im = reshape(im, 32*32, 1);  
-            data = [data im];  
+function [data, labels]=readYaleDataset(dataDir,l,readnum,n) 
+% Read images from YaleDataset 
+% Input:
+%   dataDir - path
+%   l,n - each image is a l*n matrix
+%   readnum - the number of images to read in each folder
+% Output:
+%   data - l*N*n tensor
+%   labels - N*1 vector
+dirs = dir(dataDir);
+N = readnum*(numel(dirs)-2);
+disp(N);
+data = zeros(l,N,n);  
+labels = [];  
+num=1;
+for i = 3:numel(dirs)  
+    imgDir = dirs(i).name;  
+    imgDir = fullfile(dataDir, imgDir);  
+    imgList = dir(fullfile(imgDir, '*.pgm')); 
+    for j = 1:readnum  
+        imgName = imgList(j).name;  
+        if strcmp('Ambient.pgm',  imgName(end-10:end))  
+            continue;  
         end  
-        labels = [labels ones(1, 20-1) * (i-2)];  
-%     disp('success');
+        im = imread(fullfile(imgDir, imgName));  
+        if size(im, 3) ==3  
+            im = rgb2gray(im);  
+        end  
+        im = imresize(im, [32 32]);  
+        data(:,num,:) = im;  
+        num=num+1;
     end  
-    disp('all images are saved');
-    save(saveName, 'data', 'labels');  
-end  
+    labels = [labels ones(1, readnum) * (i-2)];  
+end
+end
