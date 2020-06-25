9ffed3873ae28e870c58ec4f56d5040c0901303e
iamlsy <670076027@qq.com>
2020-06-25 21:14:09 +0800

    Update testAlgorithm2.m

--- testAlgorithm2.m
+++ testAlgorithm2.m
@@ -1,25 +1,32 @@
-function [label, Fro_norm] = testAlgorithm2(J, M, U_k, C)
-% Testing process for T-SVD Method
+function [label, Fro_norm] = testAlgorithm2(J, B, U_p)
+% Testing process for TensorFaces Method
 % Input:
 %   J   - l*n matrix
-%   M   - l*1*n tensor
-%   A   - l*N*n tensor
-%   U_k - l*k*n 'econ' form
-%   C   - k*N*n tensor
+%   B   - P*V*I*E*pix tensor
+%   U_p - P*P tensor
 % Output:
 %   label    - return the label of closest image
 %   Fro_norm - return the frobenius norm of closest image
 
-T(:,1,:) = twist(J) - M;
-B = tprod(tran(U_k),T); % k*1*n tensor
-
+% vectorize
+% T(1,1,1,1,:) = J(:); 
+[P, V, I, E, pix]=size(B.data);
 Fro_norm = +Inf; % the minimum forbenius norm 
 label = 0; % the label of minimum norm image
-for j = 1:N
-    tmp_norm = norm(B - C(:,j,:), 'fro');
-    if tmp_norm < Fro_norm
-        Fro_norm = tmp_norm;
-        label = j;
+for v=1:V
+    for i=1:I
+        for e=1:E
+            % flattened matrix B(v,i,e)
+            B_flatten=reshape(B.data(:,v,i,e,:),[P,pix]);
+            c=B_flatten*J(:);
+            for p = 1:P
+                tmp_norm = norm(c - U_p(p,:)', 'fro');
+                if tmp_norm < Fro_norm
+                    Fro_norm = tmp_norm;
+                    label = p;
+                end
+            end
+        end
     end
 end
 end
