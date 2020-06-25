6878337d015aede08960d5c70343cfb0f6d38d53
iamlsy <670076027@qq.com>
2020-06-25 18:39:13 +0800

    Update trainAlgorithm2.m

--- trainAlgorithm2.m
+++ trainAlgorithm2.m
@@ -1,7 +1,8 @@
-function [B, U_p] = trainAlgorithm2(L)
+function [B, U_p] = trainAlgorithm2(L,tol)
 % Traning process for TensorFaces Method
 % Input: 
 %   L  - P*V*I*E*pix tensor, pix = the length of each vectorized image
+%   tol - specify relative error
 % Output:
 %   U_p- P*P tensor
 %   B  - P*V*I*E*pix tensor
@@ -14,9 +15,16 @@ function [B, U_p] = trainAlgorithm2(L)
 
 
 % hosvd to get core tensor and orthogonal matrices
+[T,G,U] = hosvd(L,tol);
+Z=T.core;
+U_p=T.U{1}; 
+U_v=T.U{2};
+U_i=T.U{3};
+U_e=T.U{4};
+U_pix=T.U{5};
 
 % compute tensor B
 B=zeros(size(L));
-B=tmul(tmul(tmul(tmul(Z,U_v,2),U_i,3),U_e,4),U_pix,5);
+B=ttm(ttm(ttm(ttm(Z,U_v,2),U_i,3),U_e,4),U_pix,5); % Tensor times matrix along mode i
 
 end
