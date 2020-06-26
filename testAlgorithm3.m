229650efe975ecd0f4cd454dba5120b30110197c
iamlsy <670076027@qq.com>
2020-06-26 14:16:03 +0800

    Update testAlgorithm3.m

--- testAlgorithm3.m
+++ testAlgorithm3.m
@@ -1,7 +1,7 @@
 function [label, Fro_norm] = testAlgorithm3(J, M, U_k, C)
 % Testing process for T-SVD Method
 % Input:
-%   J   - l*n matrix
+%   J   - l*1*n matrix
 %   M   - l*1*n tensor
 %   A   - l*N*n tensor
 %   U_k - l*k*n 'econ' form
@@ -9,14 +9,14 @@ function [label, Fro_norm] = testAlgorithm3(J, M, U_k, C)
 % Output:
 %   label    - return the label of closest image
 %   Fro_norm - return the frobenius norm of closest image
-
-T(:,1,:) = twist(J) - M;
+N=size(C,2);
+T(:,1,:) = J - M;
 B = tprod(tran(U_k),T); % k*1*n tensor
 
 Fro_norm = +Inf; % the minimum forbenius norm 
 label = 0; % the label of minimum norm image
 for j = 1:N
-    tmp_norm = norm(B - C(:,j,:), 'fro');
+    tmp_norm = norm(squeeze(B - C(:,j,:)), 'fro');
     if tmp_norm < Fro_norm
         Fro_norm = tmp_norm;
         label = j;
